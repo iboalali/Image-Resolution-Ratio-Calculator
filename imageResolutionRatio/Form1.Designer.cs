2af46df179636241c46a16032d733dcb60b139b1
iboalali <alali@live.de>
2014-10-25 11:06:55 +0300

    Publish
    First Release

---
+++ imageResolutionRatio/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace imageResolutionRatio {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSideLength = new System.Windows.Forms.Label();
+            this.txtRatioHeight = new System.Windows.Forms.TextBox();
+            this.txtRatioWidth = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSide = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(118, 12);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 0;
+            this.txtWidth.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            this.txtWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(118, 38);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 1;
+            this.txtHeight.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            this.txtHeight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ratio";
+            // 
+            // lblSideLength
+            // 
+            this.lblSideLength.Location = new System.Drawing.Point(115, 90);
+            this.lblSideLength.Name = "lblSideLength";
+            this.lblSideLength.Size = new System.Drawing.Size(103, 13);
+            this.lblSideLength.TabIndex = 10;
+            this.lblSideLength.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.lblSideLength, "Click to Copy the Number");
+            this.lblSideLength.Click += new System.EventHandler(this.lblSideLength_Click);
+            // 
+            // txtRatioHeight
+            // 
+            this.txtRatioHeight.Location = new System.Drawing.Point(177, 64);
+            this.txtRatioHeight.Name = "txtRatioHeight";
+            this.txtRatioHeight.Size = new System.Drawing.Size(41, 20);
+            this.txtRatioHeight.TabIndex = 3;
+            this.txtRatioHeight.Text = "9";
+            this.txtRatioHeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtRatioHeight.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            this.txtRatioHeight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
+            // 
+            // txtRatioWidth
+            // 
+            this.txtRatioWidth.Location = new System.Drawing.Point(118, 64);
+            this.txtRatioWidth.Name = "txtRatioWidth";
+            this.txtRatioWidth.Size = new System.Drawing.Size(41, 20);
+            this.txtRatioWidth.TabIndex = 2;
+            this.txtRatioWidth.Text = "16";
+            this.txtRatioWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtRatioWidth.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            this.txtRatioWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(163, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = ":";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSide
+            // 
+            this.lblSide.AutoSize = true;
+            this.lblSide.Location = new System.Drawing.Point(12, 90);
+            this.lblSide.Name = "lblSide";
+            this.lblSide.Size = new System.Drawing.Size(0, 13);
+            this.lblSide.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 146);
+            this.Controls.Add(this.lblSide);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRatioWidth);
+            this.Controls.Add(this.txtRatioHeight);
+            this.Controls.Add(this.lblSideLength);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSideLength;
+        private System.Windows.Forms.TextBox txtRatioHeight;
+        private System.Windows.Forms.TextBox txtRatioWidth;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSide;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ imageResolutionRatio/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageResolutionRatio {
+    public partial class Form1 : Form {
+        public Form1 () {
+            InitializeComponent();
+            Icon = global::imageResolutionRatio.Properties.Resources.imageRatioIcon;
+        }
+
+        private void button1_Click ( object sender, EventArgs e ) {
+            Environment.Exit( Environment.ExitCode );
+        }
+
+        private void button2_Click ( object sender, EventArgs e ) {
+            calculate();
+        }
+
+        private void calculate () {
+            if ( txtHeight.Text == string.Empty && txtWidth.Text == string.Empty ) {
+                //MessageBox.Show( "Please input a height or width", "Empty Text Box" );
+                return;
+
+            }
+
+            if ( txtRatioHeight.Text == string.Empty || txtRatioWidth.Text == string.Empty ) {
+                return;
+
+            }
+
+            if ( txtHeight.Text != string.Empty && txtWidth.Text != string.Empty ) {
+                //MessageBox.Show( "Please input only one of them", "Too Many" );
+                return;
+
+            }
+
+            int height = 0;
+            int width = 0;
+            int ratioWidth = 0;
+            int ratioHeight = 0;
+            float ratio = 0;
+
+            if ( txtHeight.Text != string.Empty ) {
+                height = int.Parse( txtHeight.Text );
+                ratioWidth = int.Parse( txtRatioWidth.Text );
+                ratioHeight = int.Parse( txtRatioHeight.Text );
+                ratio = ( float ) ratioWidth / ( float ) ratioHeight;
+
+                width = ( int ) ( height * ratio );
+                lblSide.Text = "Width";
+                lblSideLength.Text = width.ToString();
+            }
+
+            if ( txtWidth.Text != string.Empty ) {
+                width = int.Parse( txtWidth.Text );
+                ratioWidth = int.Parse( txtRatioWidth.Text );
+                ratioHeight = int.Parse( txtRatioHeight.Text );
+                ratio = ( float ) ratioHeight / ( float ) ratioWidth;
+
+                height = ( int ) ( width * ratio );
+                lblSide.Text = "Height";
+                lblSideLength.Text = height.ToString();
+            }
+
+
+
+        }
+
+        private void txt_KeyPress ( object sender, KeyPressEventArgs e ) {
+            if ( !char.IsDigit( e.KeyChar ) && !char.IsControl( e.KeyChar ) ) {
+                e.Handled = true;
+
+            }
+
+
+
+
+        }
+
+        private void txt_TextChanged ( object sender, EventArgs e ) {
+            calculate();
+
+        }
+
+        private void lblSideLength_Click ( object sender, EventArgs e ) {
+            Clipboard.SetText( lblSideLength.Text );
+        }
+
+    }
+}
