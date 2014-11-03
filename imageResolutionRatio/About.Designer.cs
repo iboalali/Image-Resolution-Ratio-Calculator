985bb5dd6c1f0865b5fdb2dfb28aa33fc2696403
iboalali <alali@live.de>
2014-11-03 13:38:18 +0200

    Added an about screen

---
+++ imageResolutionRatio/About.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace imageResolutionRatio {
+    partial class AboutForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabelLicense = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabelEmail = new System.Windows.Forms.LinkLabel();
+            this.linkLabelPage = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hi, I\'m Ibrahim Alali";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Copyright © 2014 ibosoft";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(580, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "This application is licensed under the Apache License, Version 2.0 (the \"License\"" +
+    "); you may not use this file except in compliance with the License. You may obta" +
+    "in a copy of the License at";
+            // 
+            // linkLabelLicense
+            // 
+            this.linkLabelLicense.AutoSize = true;
+            this.linkLabelLicense.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.linkLabelLicense.Location = new System.Drawing.Point(29, 110);
+            this.linkLabelLicense.Name = "linkLabelLicense";
+            this.linkLabelLicense.Size = new System.Drawing.Size(274, 16);
+            this.linkLabelLicense.TabIndex = 3;
+            this.linkLabelLicense.TabStop = true;
+            this.linkLabelLicense.Text = "http://www.apache.org/licenses/LICENSE-2.0";
+            this.linkLabelLicense.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label4.Location = new System.Drawing.Point(12, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(514, 107);
+            this.label4.TabIndex = 4;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // linkLabelEmail
+            // 
+            this.linkLabelEmail.AutoSize = true;
+            this.linkLabelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.linkLabelEmail.Location = new System.Drawing.Point(29, 254);
+            this.linkLabelEmail.Name = "linkLabelEmail";
+            this.linkLabelEmail.Size = new System.Drawing.Size(128, 16);
+            this.linkLabelEmail.TabIndex = 5;
+            this.linkLabelEmail.TabStop = true;
+            this.linkLabelEmail.Text = "iboalali@gmail.com";
+            this.linkLabelEmail.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
+            // linkLabelPage
+            // 
+            this.linkLabelPage.AutoSize = true;
+            this.linkLabelPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.linkLabelPage.Location = new System.Drawing.Point(29, 270);
+            this.linkLabelPage.Name = "linkLabelPage";
+            this.linkLabelPage.Size = new System.Drawing.Size(81, 16);
+            this.linkLabelPage.TabIndex = 6;
+            this.linkLabelPage.TabStop = true;
+            this.linkLabelPage.Text = "iboalali.com";
+            this.linkLabelPage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(517, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(604, 341);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabelPage);
+            this.Controls.Add(this.linkLabelEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabelLicense);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "AboutForm";
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabelLicense;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabelEmail;
+        private System.Windows.Forms.LinkLabel linkLabelPage;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ imageResolutionRatio/About.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageResolutionRatio {
+    public partial class AboutForm : Form {
+        public AboutForm () {
+            InitializeComponent();
+            LinkLabel.Link linkLicense = new LinkLabel.Link();
+            linkLicense.LinkData = "http://www.apache.org/licenses/LICENSE-2.0";
+            linkLabelLicense.Links.Add( linkLicense );
+
+            LinkLabel.Link linkEmail = new LinkLabel.Link();
+            linkEmail.LinkData = "mailto:iboalali@gmail.com";
+            linkLabelEmail.Links.Add( linkEmail );
+
+            LinkLabel.Link linkPage = new LinkLabel.Link();
+            linkPage.LinkData = "http://iboalali.com";
+            linkLabelPage.Links.Add( linkPage );
+        }
+
+        private void btnClose_Click ( object sender, EventArgs e ) {
+            this.Close();
+        }
+
+        private void linkLabel_LinkClicked ( object sender, LinkLabelLinkClickedEventArgs e ) {
+            Process.Start( e.Link.LinkData as string );
+        }
+
+    }
+}
